2b6a724b65970358eca4a118026eb1b42a4e2b44
viswanath Reddy <viswanath3344@gmail.com>
2024-12-13 12:12:01 +0530

    Align the view with flex number in sign in screen. It more dynamic than arranging the marginbottom property.

---
+++ source/components/spacer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const Spacer = ({children}) => {
+  return <View style = {styles.spacer}>
+     {children}
+  </View>
+};
+
+
+const styles = StyleSheet.create({
+    spacer: {
+        margin: 16
+    }
+})
+
+export default Spacer;
--- source/screens/SigninScreen.js
+++ source/screens/SigninScreen.js
@@ -1,12 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, View} from "react-native";
+import { Button, Text, Input } from "@rneui/base";
+import Spacer from "../components/spacer";
 
-const SigninScreen = () => {
-    return <View>
-        <Text>Signin Screen </Text>
+const SigninScreen = ({navigation}) => {
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+  
+
+    return <View style = {styles.container}>
+        <View style = {{flex: 1, backgroundColor: "red"}}/>
+        <View style = {{flex: 4}}>
+        <Spacer>
+        <Text h2> Signup to Tracker</Text>
+        </Spacer>
+
+        <Input label = "Username" />
+        <Input label = "Password"/>
+        <Spacer>
+        <Button title={"Signup"}/> 
+        </Spacer>
+        </View>
+        <View style = {{flex: 1, backgroundColor: "yellow"}}/>
     </View>
 }
 
-const styleSheet = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        // justifyContent: "center",
+        // marginBottom: 250
+    }
+});
 
 export default SigninScreen;
