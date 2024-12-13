6b7f6453a46aa87138c4be47269379e2b46558d5
viswanath Reddy <viswanath3344@gmail.com>
2024-12-13 12:32:46 +0530

    1.state variables for username and password 2.Mapped state variables to input fieds 3. Enable secury input, disabled the auto correct, auto capitlization to input fields

--- source/screens/SigninScreen.js
+++ source/screens/SigninScreen.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View} from "react-native";
 import { Button, Text, Input } from "@rneui/base";
 import Spacer from "../components/spacer";
 
 const SigninScreen = ({navigation}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     useEffect(() => {
         navigation.setOptions({
@@ -11,14 +13,26 @@ const SigninScreen = ({navigation}) => {
         })
     }, [])
   
-
     return <View style = {styles.container}>
         <Spacer>
-        <Text h2> Signup to Tracker</Text>
+        <Text h3 marginBottom = {20}> Signup to Tracker</Text>
         </Spacer>
 
-        <Input label = "Username" />
-        <Input label = "Password"/>
+        <Input label = "Username" 
+        value= {username} 
+        onChangeText={setUsername} 
+        autoCapitalize="none"
+        autoCorrect = {false}
+        />
+
+        <Input 
+        label = "Password" 
+        value= {password} 
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect = {false}
+        secureTextEntry
+        />
         <Spacer>
         <Button title={"Signup"}/> 
         </Spacer>
