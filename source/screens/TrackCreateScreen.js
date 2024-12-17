1f6dfb3737b2d8eb671dc7c77b5d4320f0a8e537
viswanath Reddy <viswanath3344@gmail.com>
2024-12-17 20:26:19 +0530

    Removed extra app.json.
    Getting the current location using express location

--- source/screens/TrackCreateScreen.js
+++ source/screens/TrackCreateScreen.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-
+import * as Location from 'expo-location';
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+    const [location, setLocation] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(()=> {
+      const getCurrentLocation = async () => {
+        let permission = await Location.requestForegroundPermissionsAsync();
+        console.log(permission);
+        if(permission.status !== 'granted') {
+         setErrorMessage("Permission to access location denied");
+         return
+        }
+
+        let location = await Location.getCurrentPositionAsync();
+        console.log(location);
+        setLocation(location);
+      }
+      
+      getCurrentLocation();
+
+    }, [])
+
     return <SafeAreaView style = {styles.container}>
         <Text>Track Create Screen </Text>
+        {location != null ? <Text>{`${location.coords.latitude}, ${location.coords.longitude}`}</Text> : null }
         <Map />
+        {errorMessage != null ? <Text style = {{color:'red', fontSize: 16}}>{errorMessage}</Text> : null}
     </SafeAreaView>
 }
 
